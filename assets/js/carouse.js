7aca2fecdda73b4db844e14abcd3e77ef4e6cedf
abdulloh01 <abdulloh04062008gmail.com>
2023-11-09 08:22:25 +0500

    first comment

---
+++ assets/js/carouse.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            loop:true,
+            margin:10,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                1260:{
+                    items:2,
+                    nav:true,
+                    loop:false
+                }
+            }
+        }
+    );
+  });
