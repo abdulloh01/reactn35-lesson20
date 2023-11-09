3629483697d6d972b3597de613fc05505be5324a
abdulloh01 <abdulloh04062008gmail.com>
2023-11-09 14:11:29 +0500

    first comment

--- assets/js/carouse.js
+++ assets/js/carouse.js
@@ -18,3 +18,20 @@ $(document).ready(function(){
         }
     );
   });
+
+
+//   navbar shrink
+
+window.addEventListener("scroll", function () {
+    shrink();
+  });
+  
+  let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (scrollY > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+          navbar.classList.remove("navbar-shrink");
+      }
+  }
